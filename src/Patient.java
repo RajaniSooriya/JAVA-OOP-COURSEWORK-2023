e03cf83f215bc48178e6b941d953add80f3e5fec
Rajani Sooriyaarachchi <95573031+RajaniSooriya@users.noreply.github.com>
2023-01-08 03:12:40 +0530

    add the comments

--- src/Patient.java
+++ src/Patient.java
@@ -1,22 +1,27 @@
 import java.time.LocalDate;
 
-public class Patient extends Person {
+public class Patient extends Person { //patient is a subclass of the super class person
+                                        //-----------INHERITANCE OOP CONCEPT FOLLOWS HERE----------------
     // instance variables
-    private String id;
+    private String id; // restricted access to this class attribute
 
-    public Patient(String name, String surname, LocalDate dob, String mobileNumber) {
+    public Patient(String name, String surname, LocalDate dob, String mobileNumber) { //parameterized constructor
         super(name, surname, dob, String.valueOf(mobileNumber));
         this.id = id;
     }
 
-    public Patient() {
+    public Patient() { //no  arg constructor
 
     }
 
     // getters and setters for instance variables
+
+    //get method for Id
     public String getId() {
         return id;
     }
+
+    //set method for id
 
     public void setId(String id) {
         this.id = id;
--- src/Person.java
+++ src/Person.java
@@ -1,53 +1,63 @@
-import java.time.LocalDate;
+import java.time.LocalDate; //to display the current date using this import statement
 
 public class Person {               //Person is a superclass
     // instance variables
     private String firstName;       //these four variables are restricted using the private access modifier
                                     //---------------ENCAPSULATION METHOD IS BEING USED HERE ------------------------------------
     private String surname;         //restricted access
-    private LocalDate dob;
-    private String mobileNumber;
+    private LocalDate dob;          //restricted access
+    private String mobileNumber;    //restricted access
 
-    public Person(String firstName, String surname, LocalDate dob, String mobileNumber) {
+    //this parameterized constructor initialize the attributes of the class
+    public Person(String firstName, String surname, LocalDate dob, String mobileNumber) { //this is a parameterized constructor
         this.firstName = firstName;
         this.surname = surname;
         this.dob = dob;
         this.mobileNumber = mobileNumber;
     }
 
-    public Person() {
+    public Person() { //this is a non arg constructor ,this is made to call the super class constructor
 
     }
 
-    // getters and setters for instance variables
+    //get methods and set methods are provided for to access the value of the private variables
+
+    //get method for firstName
     public String getFirstName() {
         return firstName;
     }
 
+    //set method for firstName
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    //get method for surname
     public String getSurname() {
         return surname;
     }
 
+    //set method for surname
     public void setSurname(String surname) {
         this.surname = surname;
     }
 
+    //get method for date
     public LocalDate getDob() {
         return dob;
     }
 
+    //set method for date
     public void setDob(LocalDate dob) {
         this.dob = dob;
     }
 
+    //get method for mobileNumber
     public String getMobileNumber() {
         return mobileNumber;
     }
 
+    //set method for mobileNumber
     public void setMobileNumber(String mobileNumber) {
         this.mobileNumber = mobileNumber;
     }
